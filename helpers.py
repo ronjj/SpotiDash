7d8775baaf5a27cfffa8bf1ba1bb95e28f685567
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2022-12-16 13:47:17 -0500

    first commit

---
+++ helpers.py
@@ -0,0 +1,60 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+import json
+
+
+#Helper Functions to get information on the currently playing song
+
+
+SPOTIPY_CLIENT_ID = 'e28a6e30300349439d3a8383b8304ac2'
+SPOTIPY_CLIENT_SECRET = 'd0ef0a7539dd4202bda55ed6b4f81497'
+SPOTIPY_REDIRECT_URI = 'https://www.google.com'
+
+scope = 'user-read-currently-playing'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI ))
+
+current = sp.currently_playing()
+
+#Prints Out Whole Response In A Nice Way
+#print(json.dumps(current, sort_keys=False, indent = 4))
+
+def is_playing():
+    """Returns True if song is playing, False if a song is not currently playing"""
+    result = current['is_playing']
+    print(result)
+    return result
+
+def get_currently_playing():
+    """Returns a string of the currently playing song"""
+   
+    artist_name = current['item']['album']['artists'][0]['name']
+    song_title = current['item']['name']
+
+    result = f'{song_title} by {artist_name} is playing'
+    print(result)
+    return result
+
+def get_currently_playing_uri():
+    link = current['item']['external_urls']['spotify']
+
+    print(link)
+    return link
+
+get_currently_playing()
+is_playing()
+
+
+
+
+
+# spotipy_client_id = os.environ['SPOTIPY_CLIENT_ID']
+# spotipy_secret = os.environ['SPOTIPY_CLIENT_SECRET']
+# spotipy_redirect_uri = os.environ['SPOTIPY_REDIRECT_URI']
+
+# scope = "user-library-read"
+
+# results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
---
+++ main.py
@@ -0,0 +1,42 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+import json
+
+#Main Dashboard Information
+
+SPOTIPY_CLIENT_ID = 'e28a6e30300349439d3a8383b8304ac2'
+SPOTIPY_CLIENT_SECRET = 'd0ef0a7539dd4202bda55ed6b4f81497'
+SPOTIPY_REDIRECT_URI = 'https://www.google.com'
+
+NUMBER_OF_SONGS = 5
+
+scope = 'user-library-read'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI ))
+current = sp.current_user_saved_albums(limit=NUMBER_OF_SONGS)
+
+#Prints Out Whole Response In A Nice Way
+# print(json.dumps(current['items'], sort_keys=False, indent = 4))
+
+
+def get_most_recent_albums():
+    acc = []
+    for x in range(NUMBER_OF_SONGS):
+        
+        artist_name = current['items'][x]['album']['artists'][0]['name']
+        album_name = current['items'][x]['album']['name']
+        date_added = current['items'][x]['added_at']
+        date_added = date_added[:-10]
+
+        print(f'{x+1}: {album_name}, {artist_name}. Added on {date_added}')
+        acc.append(f'{x+1}: {album_name}, {artist_name}. Added on{date_added}')
+
+    return acc
+
+get_most_recent_albums()
+
+
+
+
+
+    
